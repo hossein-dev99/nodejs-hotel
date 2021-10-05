2f085a459d805283d5060049b4f4ae5b4e9c769a
hossein molapour <hossein.molapour@yahoo.com>
2021-10-05 13:18:48 +0330

    add mongo atlas

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
-const {MONGO_PORT ,MONGO_HOST ,MONGO_DB} = process.env;
+const {MONGO_PORT ,MONGO_HOST ,MONGO_DB ,MONGO_USER ,MONGO_PASS} = process.env;
 
 mongoose.connection.once("open" ,() => {
     console.log(`MongoDB running successfully...`);
 });
 
 const connect = async () => {
-    await mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`);
+    await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@hotel-cluster.vdcvr.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`);
 }
 
 exports.run = () => {
     connect().catch(e => {
-        console.log(`MongoDB has an error!`);
+        console.log("MongoDb has an error: ");
+        console.log(e);
     })
 }
